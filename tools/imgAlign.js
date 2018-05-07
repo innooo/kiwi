f2c0f6f74143f3d2b4ee29a672bdac22dcfd30e5
unknown <wb-dsy346555@alibaba-inc.com>
2018-05-07 17:34:56 +0800

    'commpleteEdit'

--- tools/imgAlign.js
+++ tools/imgAlign.js
@@ -11,9 +11,7 @@ function alignImg(img, container, cb) { // img: 源图片  container: canvas画�
   var imgH = img.height;
   var imgRatio = imgW/imgH;
 
-  var startX, startY, width, height; // 画布绘制图片的参数
-
-  if(imgRatio > containerRatio) { // drawImage(rowImage, startX, startY, width, height)
+  if(imgRatio > containerRatio) { 
     cb(0, (containerH - imgH/imgW*containerW)/2, containerW, imgH/imgW*containerW);
   } else {
     cb((containerW - imgW/imgH*containerH)/2, 0, imgW/imgH*containerH, containerH);
@@ -24,12 +22,11 @@ function alignImg(img, container, cb) { // img: 源图片  container: canvas画�
 function preImage(imgURL, cb) {
   var img = new Image();
   img.src = imgURL;
-
-  if(img.complate) { // 如果图片已经存在缓存中
+  if(img.complete ) { // 如果图片已经存在缓存中 (可以很大得提升性能)
       cb.call(img); // 执行回调函数，并且将img对象作为回调函数的this进行执行
       return;
   }
   img.onload = () => { // 首次加载图片，等图片加载完成执行回调函数
-      cb.call(img);
+    cb.call(img);
   }
 }
